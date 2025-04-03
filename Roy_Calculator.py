d83c77db28604df9597125df9ce6fb48467d0633
RoyMakanjira <roymakanjira@gmail.com>
2025-04-03 13:37:59 +0200

    firt commit

---
+++ Roy_Calculator.py
@@ -0,0 +1,37 @@
+#  Roy the Makanjira calculater
+def subtraction(r , y):
+    return r - y
+
+def addition(r , y):
+    return r + y
+
+def multiplication(r , y):
+    return r * y
+
+def division(r , y):
+    if y == 0:
+     return "it can't ERROR !!!"
+    return r / y
+
+
+print("How do you want to calculate")
+print("1. Subtraction")
+print("2. Addition")
+print("3.Multiplication")
+print("4.Division")
+
+choice = input("Enter How you want to calculate (1/2/3/4): ")
+
+number_1 = int(input("Put the first number"))
+number_2 = int(input("put the second number"))
+
+if choice == "1":
+   print(f"the result is: {subtraction(number_1,number_2)} ")
+elif choice == "2":
+   print(f"the result is: {addition(number_1,number_2)} ")
+elif choice == "3":
+   print(f"the result is :{multiplication(number_1,number_2)}")
+elif choice == "4":
+   print(f"the result is : {division(number_1,number_2)}")
+else:
+   print("invalid !!!!!!!!!!!!!")
---
+++ Thursday_April.py
@@ -0,0 +1,15 @@
+# BODMAS BASED CALCULATOR
+
+def add(x,y):
+    return x + y
+
+def subtract(x,y):
+    return x - y
+
+def multiply(x,y):
+    return x * y
+
+def division(x,y):
+    return x / y
+
+
